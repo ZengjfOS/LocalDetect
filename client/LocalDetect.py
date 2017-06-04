700531f28fca2be0068cbba2b74969162562b9a5
ZoroZeng <aplexos.@163.com>
2017-06-04 20:14:43 +0800

    modify:change localDetect to current demand.

--- client/LocalDetect.py
+++ client/LocalDetect.py
@@ -2,13 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import socket
-import time
 import os
+import sys
 
-runWhileTimes = 0
-
-while True:
-    runWhileTimes += 1
+def main():
 
     mac = "";
     for line in os.popen("/sbin/ifconfig"):
@@ -16,66 +13,29 @@ while True:
             mac = line.split()[1]
             break
 
-    address = ('255.255.255.255', 50000)
-    socketClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    socketClient.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    try:
-        socketClient.sendto(mac.encode('utf-8'), address)
-    except:
-        print("Please check your network.")
-        time.sleep(1)
-        continue
+    while True:
 
-    address = ('0.0.0.0', 50001)
-    socketServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    socketServer.settimeout(3);
-    socketServer.bind(address)
+        address = ('0.0.0.0', 50000)
+        socketServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        socketServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        socketServer.setblocking(True)
+        socketServer.bind(address)
 
-    TCPPort = "";
-    addr = ""
-    try:
-        TCPPort, addr = socketServer.recvfrom(2048)
-        print("received:", TCPPort, "from", addr)
-    except:
-        print("client has exist")
-        continue
-
-    clientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientSock.settimeout(1)
-    try:
-        print("%s, %d" % (addr[0], int(TCPPort.decode("utf-8"))))
-        clientSock.connect((addr[0], int(TCPPort.decode("utf-8"))))
-    except:
-        print("connect failed.")
-        continue
-
-    sendDataCount = 0
-    while 1:
         try:
-            sendlen = clientSock.send(bytes("ok", encoding="utf-8"))
-            try:
-                recvData = clientSock.recv(1024)
-            except:
-                print("receive data after ")
+            data, addr = socketServer.recvfrom(2048)
+            # print("received:", data, "from", addr)
+        except ValueError:
+            print(ValueError)
+            continue
 
-            print("runWhileTimes(%d) send data (%d) after %d" % (runWhileTimes, sendDataCount, sendlen))
+        address = (addr[0], 50001)
+        # print(address)
+        socketClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            socketClient.sendto(mac.encode(), address)
+        except ValueError:
+            print(ValueError)
+            continue
 
-            # sendDataCount += 1
-            # if sendDataCount > 10 :
-            #     break
-
-            time.sleep(1)
-        except:
-            print("send data failed.")
-            break;
-
-    print("disconnet server")
-    clientSock.close()
-
-    time.sleep(5)
-    print("over")
-
-socketClient.close()
-
-# if __name__ == "__main__" :
-#     sys.exit(main())
+if __name__ == "__main__":
+    sys.exit(main())
