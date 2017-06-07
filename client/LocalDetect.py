a8b24aa38162bc72ca169630ce23ddd701d5a6e6
ZoroZeng <aplexos.@163.com>
2017-06-07 12:31:52 +0800

    modify: make python local detect to a module.

--- client/LocalDetect.py
+++ client/LocalDetect.py
@@ -3,39 +3,43 @@
 
 import socket
 import os
-import sys
+import threading
 
-def main():
 
-    mac = "";
-    for line in os.popen("/sbin/ifconfig"):
-        if 'ether' in line:
-            mac = line.split()[1]
-            break
+class LocalDetect(threading.Thread):
 
-    while True:
+    @classmethod
+    def run(cls):
 
-        address = ('0.0.0.0', 50000)
-        socketServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        socketServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        socketServer.setblocking(True)
-        socketServer.bind(address)
+        mac = "";
+        for line in os.popen("/sbin/ifconfig"):
+            if 'ether' in line:
+                mac = line.split()[1]
+                break
 
-        try:
-            data, addr = socketServer.recvfrom(2048)
-            # print("received:", data, "from", addr)
-        except ValueError:
-            print(ValueError)
-            continue
+        while True:
 
-        address = (addr[0], 50001)
-        # print(address)
-        socketClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        try:
-            socketClient.sendto(mac.encode(), address)
-        except ValueError:
-            print(ValueError)
-            continue
+            address = ('0.0.0.0', 50000)
+            socketServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            socketServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            socketServer.setblocking(True)
+            socketServer.bind(address)
+
+            try:
+                data, addr = socketServer.recvfrom(2048)
+                # print("received:", data, "from", addr)
+            except ValueError:
+                print(ValueError)
+                continue
+
+            address = (addr[0], 50001)
+            # print(address)
+            socketClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            try:
+                socketClient.sendto(mac.encode(), address)
+            except ValueError:
+                print(ValueError)
+                continue
 
 if __name__ == "__main__":
-    sys.exit(main())
+    LocalDetect().start()
